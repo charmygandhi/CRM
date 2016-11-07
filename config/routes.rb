1b324cc591e81743f14117d6da905552cf3afc2c
Charmy Gandhi <charmygandhi93@gmail.com>
2016-11-07 15:49:27 -0600

    Added new controller and css for

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'finder/index'
+
+  get 'finder/missing_email'
+
+  get 'finder/alphabetized'
+
+  root to: 'finder#index'
+
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
